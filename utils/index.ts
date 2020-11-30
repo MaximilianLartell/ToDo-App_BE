6cd9720d8994b142784a7db30e416a3eb0bd419b
Maximilian Skoglund Lartell <max.lartell@gmail.com>
2020-11-30 13:19:25 +0100

    .env added to gitignore

--- utils/index.ts
+++
@@ -1,9 +0,0 @@
-// this will contain helper functions
-import { Error } from '../types';
-
-export const error = (code: number, message: string): Error => ({
-  code,
-  error: message,
-});
-
-export const NewUtil = 'coming';
